a664fc64d69329e3968395c1570af56769d56a39
Supratik Das <rick.das08@gmail.com>
2020-06-28 16:59:02 +0530

    fix host

--- app.py
+++ app.py
@@ -225,7 +225,7 @@ def delete_campaign():
 ##################################################################################
 
 if __name__ == "__main__":
-    app.run(threaded=True, port=5000)
+    app.run(threaded=True, port=5000, host='0.0.0.0')
 
 
 
